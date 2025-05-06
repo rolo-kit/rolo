dea0f022badfd4630f32a6ceb945800198704b82
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2025-05-06 21:15:59 +0530

    rolo: add react template for project scaffolding

---
+++ packages/cli/templates/react/src/popup/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+      <div style={{ padding: 12, fontFamily: 'sans-serif' }}>
+        <h1>🔧 Rolo Extension</h1>
+        <p>This is a popup built with React!</p>
+      </div>
+    )
+  }
+  
