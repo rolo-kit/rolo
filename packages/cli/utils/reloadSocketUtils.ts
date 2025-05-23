3260c5684a2bb3bc8583c5b6bd5cacc2c9343d3d
Mohammed Rayan Sailani <rayanbackup1@gmail.com>
2025-05-23 18:43:37 +0530

    fix: improve type annotations in reloadSocketUtils

--- packages/cli/utils/reloadSocketUtils.ts
+++ packages/cli/utils/reloadSocketUtils.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 
 export function setupWebsocketConnectionForReload() {
   const PORT_NO = 35729;
@@ -16,7 +16,7 @@ export function closeWebsocketServer(wss: WebSocketServer) {
 }
 
 export function notifyClients(wss: WebSocketServer) {
-  wss.clients.forEach((client) => {
+  wss.clients.forEach((client: WebSocket) => {
     if (client.readyState === 1) {
       client.send('reload');
     }
